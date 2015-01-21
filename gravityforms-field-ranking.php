1ec4552a06975f31843b896d0aa04496966dc0c2
Laurens Offereins <lmoffereins@gmail.com>
2015-01-21 13:36:28 +0100

    First attempt at input field rendering. Styles should be parted later

--- gravityforms-field-ranking.php
+++ gravityforms-field-ranking.php
@@ -111,6 +111,9 @@ final class GravityForms_Field_Ranking {
 		// Set field title
 		add_filter( 'gform_field_type_title', array( $this, 'set_field_title' ) );
 
+		// Render field input
+		add_filter( 'gform_field_input', array( $this, 'render_field_input' ), 10, 5 );
+
 		// Field classes
 		add_filter( 'gform_field_css_class', array( $this, 'field_classes' ), 10, 2 );
 	}
@@ -152,11 +155,135 @@ final class GravityForms_Field_Ranking {
 
 		// For Ranking fields, name accordingly
 		if ( $this->type === $type ) {
-			$type = __( 'Ranking', 'gravityforms-field-ranking' );
+			$title = __( 'Ranking', 'gravityforms-field-ranking' );
+		} else {
+			$title = $type;
 		}
 
-		return $type;
+		return $title;
 	}
+
+	/**
+	 * Return the field's input markup
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @param string $input Field's input markup
+	 * @param array $field Field data
+	 * @param string $value Field's value
+	 * @param int $lead_id Lead ID
+	 * @param int $form_id Form ID
+	 * @return string Field's input markup
+	 */
+	public function render_field_input( $input, $field, $value, $lead_id, $form_id ) {
+
+		// Bail when not rendering a Ranking field
+		if ( $this->type !== $field['type'] )
+			return $input;
+
+		// Define input attributes
+		$name     = 'input_' . $form_id . '_' . $field['id'] . '[]';
+		$class    = isset( $field['cssClass'] ) ? esc_attr( $field['cssClass'] ) : '';
+		$tabindex = GFCommon::get_tabindex();
+		$arrow    = ! isset( $field['arrowType'] ) || ! in_array( $field['arrowType'], array( 'default', 'alt', 'alt2' ) ) ? 'default' : $field['arrowType'];
+		$arrow    = ( 'default' != $arrow ) ? '-' . $arrow : '';
+
+		// Start output buffer
+		ob_start(); ?>
+
+		<div class="ginput_container">
+			<ol class="gfield_ranking sortable">
+				<?php foreach ( $this->get_field_options( $field, $value ) as $option_key => $option_label ) : ?>
+				<li class="sortable-item" data-value="<?php echo esc_attr( $option_key ); ?>">
+					<i class="dashicons dashicons-arrow-up<?php echo $arrow; ?>"></i><i class="dashicons dashicons-arrow-down<?php echo $arrow; ?>"></i>
+					<span class="item-label"><?php echo $option_label; ?></span>
+					<input type="hidden" name="<?php echo $name; ?>" value="<?php echo $option_key; ?>"/>
+				</li>
+				<?php endforeach; ?>
+			</ol>
+		</div>
+
+		<style>
+			.gfield_ranking {
+				margin: 6px 0;
+				list-style: none;
+				counter-reset: gfield-ranking-counter;
+			}
+			#gform_fields .gfield_ranking li {
+				margin: 0 0 6px;
+				padding: 0;
+				counter-increment: gfield-ranking-counter;
+			}
+			.gfield_ranking li i {
+				padding: 2px 0;
+				color: #888;
+				cursor: pointer;
+			}
+			.gfield_ranking li:first-of-type i:first-of-type,
+			.gfield_ranking li:last-of-type i:last-of-type {
+				color: #d5d5d5;
+				cursor: inherit;
+			}
+				.gfield_ranking i:hover {
+					background-color: #eee;
+				}
+			.gfield_ranking li .item-label {
+				cursor: move;
+				margin: 0 0 0 10px;
+			}
+			.gfield_ranking li .item-label:before {
+				content: counter(gfield-ranking-counter) ".";
+				margin: 0 5px 0 0;
+			}
+		</style>
+
+		<?php
+
+		// End output buffer
+		$input = ob_get_clean();
+
+		return apply_filters( 'gravityforms_field_ranking_field_input', $input, $field, $value, $lead_id, $form_id );
+	}
+
+		/**
+		 * Return the input options for the given field
+		 *
+		 * @since 1.0.0
+		 * 
+		 * @param array $field Field data
+		 * @param string $value Field input value
+		 * @return array Field options
+		 */
+		public function get_field_options( $field, $value ) {
+
+			// Field has no options
+			if ( ! isset( $field['options'] ) ) {
+				$field['options'] = array( 
+					1 => __( 'First Option',  'gravityforms-field-ranking' ),
+					2 => __( 'Second Option', 'gravityforms-field-ranking' ),
+					3 => __( 'Third Option',  'gravityforms-field-ranking' )
+				);
+			}
+
+			// Align options to ranked value
+			if ( ! empty( $value ) ) {
+				if ( ! is_array( $value ) ) {
+					$value = explode( ',', $value );
+				}
+
+				// Sort options array by value order. http://stackoverflow.com/a/348418/3601434
+				$ordered = array();
+				foreach ( $value as $key ) {
+					if ( array_key_exists( $key, $field['options'] ) ) {
+						$ordered[ $key ] = $field['options'][ $key ];
+						unset( $field['options'][ $key ] );
+					}
+				}
+				$field['options'] = $ordered + $field['options'];
+			}
+
+			return $field['options'];
+		}
 
 	/**
 	 * Manipulate the field's class names
